3ee21421dbe2801e3ee893cc56eb60c91d4ed883
Hayden <igluvjh@gmail.com>
2025-12-23 15:18:17 +0900

    feat: 제품 조회 로직들에 레디스 캐시를 적용

---
+++ src/main/kotlin/kr/hhplus/be/server/application/event/ProductCacheEvictEvent.kt
@@ -0,0 +1,9 @@
+package kr.hhplus.be.server.application.event
+
+/**
+ * 상품 관련 캐시 무효화를 트리거하는 이벤트.
+ * 트랜잭션 커밋 후에 캐시를 무효화하기 위해 사용된다.
+ */
+data class ProductCacheEvictEvent(
+    val reason: String,
+)
